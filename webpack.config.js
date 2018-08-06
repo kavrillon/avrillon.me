5d94e4518bbb8043b1f15122de09e7f5b42230a3
Kevin Avrillon <kavrillon@leadformance.com>
2018-08-06 20:45:13 +0200

    chore(): build dist scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,43 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  devtool: "source-map",
+  entry: ["./src/index.js", "./src/index.scss"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader", // translates CSS into CommonJS
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader", // compiles Sass to CSS, using Node Sass by default
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
   plugins: [
+    new ExtractTextPlugin({
+      filename: "bundle.css",
+      allChunks: true
+    }),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "src/index.html"
