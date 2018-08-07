1cf8ec7e40705219330805133ddd456438220af2
Kevin Avrillon <kavrillon@leadformance.com>
2018-08-07 10:34:59 +0200

    chore(): add hash to prod js

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     ]
   },
   output: {
-    filename: "bundle.js",
+    filename: devMode ? "bundle.js" : "bundle.[hash].js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
