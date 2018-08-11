c59290763c13103c38f8c86deded9a30b7eec945
Kevin Avrillon <kavrillon@leadformance.com>
2018-08-11 11:49:38 +0200

    chore(deploy): deploy given tag instead of current branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,15 +9,25 @@ then
   exit 0
 fi
 
-echo ""
-echo "##-- Get last remote sources from $branch --##"
+if [ $# -eq 0 ]
+then
+    echo ""
+    echo "usage: deploy.sh <tag>"
+    echo ""
+    exit 0
+fi
 
-git checkout $branch
+tag=$1
+
+echo ""
+echo "##-- Get last remote sources from $tag --##"
+
+git checkout $tag
 git fetch
-git rebase $branch
+git rebase $tag
 
 echo ""
-echo "##-- Building last sources from $branch --##"
+echo "##-- Building last sources from $tag --##"
 
 yarn build:prod
 
@@ -27,6 +37,6 @@ echo "##-- Deploying on Github Pages --##"
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f dist
-git commit -m "Rebuild GitHub pages"
+git commit -m "GitHub Pages Deployment: $tag"
 git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
 git checkout $branch
