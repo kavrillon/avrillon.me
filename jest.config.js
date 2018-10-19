c68865b4ebfb7c6119317d912b8daeb7343e5177
Kevin Avrillon <kavrillon@leadformance.com>
2018-10-19 13:52:15 +0200

    chore(ci): add e2e tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  globalSetup: "jest-environment-puppeteer/setup",
+  globalTeardown: "jest-environment-puppeteer/teardown",
+  testEnvironment: "jest-environment-puppeteer"
+};
