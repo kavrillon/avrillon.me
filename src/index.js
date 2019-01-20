f74c55795fcd7171320822d40f0e58a0e38cbb26
Kevin Avrillon <kevin@avrillon.me>
2019-01-20 22:46:52 +0100

    style(): global redesign

--- src/index.js
+++ src/index.js
@@ -4,87 +4,28 @@ var ALERT_TIMER;
 
 // Load
 window.onload = function init() {
-  loadImages();
-
   loadEvents();
 };
 // Methods
 
-function loadImages() {
-  var win, doc, img, header, enhancedClass;
-  // Quit early if older browser (e.g. IE8).
-  if (!("addEventListener" in window)) {
-    return;
-  }
-
-  win = window;
-  doc = win.document;
-  img = new Image();
-  header = doc.querySelector(".splash");
-  enhancedClass = "splash--loaded";
-
-  // Rather convoluted, but parses out the first mention of a background
-  // image url for the enhanced header, even if the style is not applied.
-  var bigSrc = (function() {
-    // Find all of the CssRule objects inside the inline stylesheet
-    var styles = doc.querySelector("style").sheet.cssRules;
-    // Fetch the background-image declaration...
-    var bgDecl = (function() {
-      // ...via a self-executing function, where a loop is run
-      var bgStyle,
-        i,
-        l = styles.length;
-      for (i = 0; i < l; i++) {
-        // ...checking if the rule is the one targeting the
-        // enhanced header.
-        if (
-          styles[i].selectorText &&
-          styles[i].selectorText === "." + enhancedClass + "::before"
-        ) {
-          // If so, set bgDecl to the entire background-image
-          // value of that rule
-          bgStyle = styles[i].style.backgroundImage;
-          // ...and break the loop.
-          break;
-        }
-      }
-      // ...and return that text.
-      return bgStyle;
-    })();
-    // Finally, return a match for the URL inside the background-image
-    // by using a fancy regex i Googled up, if the bgDecl variable is
-    // assigned at all.
-    return bgDecl && bgDecl.match(/(?:\(['|"]?)(.*?)(?:['|"]?\))/)[1];
-  })();
-
-  // Assign an onLoad handler to the dummy image *before* assigning the src
-  img.onload = function() {
-    header.className += " " + enhancedClass;
-  };
-  // Finally, trigger the whole preloading chain by giving the dummy
-  // image its source.
-  if (bigSrc) {
-    img.src = bigSrc;
-  }
-}
-
 function loadEvents() {
-  var ctaContact = document.getElementById("CTA_Contact");
+  var ctaEmail = document.querySelector("[data-cta-email]");
+  var body = document.querySelector(".home");
+
+  ctaEmail.addEventListener("keypress", function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      launchCopy(ctaEmail);
+    }
+  });
+
+  ctaEmail.addEventListener("click", function() {
+    launchCopy(ctaEmail);
+  });
 
   setTimeout(function() {
-    ctaContact.classList.add("copyable");
-    ctaContact.setAttribute("tabindex", "0");
-    ctaContact.addEventListener("keypress", function(e) {
-      var key = e.which || e.keyCode;
-      if (key === 13) {
-        launchCopy(ctaContact);
-      }
-    });
-
-    ctaContact.addEventListener("click", function() {
-      launchCopy(ctaContact);
-    });
-  }, 1000);
+    body.classList.add("loaded");
+  }, 3000);
 }
 
 function launchCopy(button) {
--- test/e2e/snapshot.spec.js
+++ test/e2e/snapshot.spec.js
@@ -18,38 +18,48 @@ describe("Snapshot", () => {
     await page.goto(url);
   });
 
-  describe("Mobile", () => {
+  describe("1-Mobile", () => {
     beforeAll(async () => {
-      page.emulate(devices["iPhone 4"]);
-      await page.waitFor(3000);
+      page.emulate(devices["iPhone 5"]);
     });
 
-    it("should match on init", async () => {
+    it("1-should match on init", async () => {
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot(screenshotConfig);
+    });
+
+    it("2-should match on home loaded", async () => {
+      await page.waitFor(5100);
       const image = await page.screenshot();
       expect(image).toMatchImageSnapshot(screenshotConfig);
     });
   });
 
-  describe("Desktop", () => {
+  describe("2-Desktop", () => {
     beforeAll(async () => {
       await page.setViewport({ width: 1440, height: 900 });
-      await page.waitFor(3000);
     });
 
-    it("should match on init", async () => {
+    it("1-should match on init", async () => {
       const image = await page.screenshot();
       expect(image).toMatchImageSnapshot(screenshotConfig);
     });
 
-    it("should match when focusing action", async () => {
+    it("2-should match on home loaded", async () => {
+      await page.waitFor(5100);
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot(screenshotConfig);
+    });
+
+    it("3-should match when focusing action", async () => {
       await page.focus("[data-cta]");
       await page.waitFor(100);
       const image = await page.screenshot();
       expect(image).toMatchImageSnapshot(screenshotConfig);
     });
 
-    it("should match when focusing email", async () => {
-      await page.focus("#CTA_Contact");
+    it("4-should match when focusing email", async () => {
+      await page.focus("[data-cta-email]");
       await page.waitFor(100);
       const image = await page.screenshot();
       expect(image).toMatchImageSnapshot(screenshotConfig);
