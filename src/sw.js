b5f42bea4b1edb2608ef47971434befe5612c916
Kevin Avrillon <kevin@avrillon.me>
2019-03-04 09:21:20 +0100

    feat(pwa): add service worker

---
+++ src/sw.js
@@ -0,0 +1,24 @@
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.setConfig({
+  debug: false,
+});
+
+workbox.routing.registerRoute(/\.(?:js|css|html)$/, new workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.googleAnalytics.initialize();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
