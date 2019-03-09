faa9336fef913c30af83cc0ea8df83adc36eacd8
Kevin Avrillon <kevin@avrillon.me>
2019-03-09 12:07:58 +0100

    chore(): generate files for version 2.2.1

---
+++ dist/precache-manifest.e3f90cc62be8bb0f20d751a399ffd71b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da918a3e1f43fe4e560",
+    "url": "js/app.1f6a7b6.js"
+  },
+  {
+    "revision": "4ccff86fc14ab8d22135",
+    "url": "js/critical.1f6a7b6.js"
+  },
+  {
+    "revision": "4ccff86fc14ab8d22135",
+    "url": "css/critical.1f6a7b6.css"
+  },
+  {
+    "revision": "454f62b4ef762054be136ff16f381a57",
+    "url": "img/apple-touch-icon.454f62b.png"
+  },
+  {
+    "revision": "f6b5394d6b27d2c620d24762dc1abdef",
+    "url": "img/favicon-32x32.f6b5394.png"
+  },
+  {
+    "revision": "8fcca59ccb3f88d81f03eb3cd98886e4",
+    "url": "img/favicon-16x16.8fcca59.png"
+  },
+  {
+    "revision": "3dba4812afa26e9d7aea8c3096da3cb8",
+    "url": "img/safari-pinned-tab.3dba481.svg"
+  },
+  {
+    "revision": "95bcc17d5781887ae004ffdc6963b832",
+    "url": "fonts/montserrat-v12-latin-200.eot"
+  },
+  {
+    "revision": "29c1d31f7d9bc4f5c1841eb14fbf5cd7",
+    "url": "fonts/montserrat-v12-latin-regular.eot"
+  },
+  {
+    "revision": "040482ce0f8a3aee1f0568d9c50ba55d",
+    "url": "fonts/montserrat-v12-latin-700.eot"
+  },
+  {
+    "revision": "444ae007121264bc1969d49b4031f9b2",
+    "url": "fonts/montserrat-v12-latin-200.woff2"
+  },
+  {
+    "revision": "edbce16a90aa22c297a0307b85789837",
+    "url": "fonts/montserrat-v12-latin-200.woff"
+  },
+  {
+    "revision": "52f4c7bb2db43387e6a03d82d2cb9be5",
+    "url": "fonts/montserrat-v12-latin-200.ttf"
+  },
+  {
+    "revision": "c4bce4a7535147c26ff98c30cf7f045d",
+    "url": "img/montserrat-v12-latin-200.c4bce4a.svg"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "fonts/montserrat-v12-latin-regular.woff2"
+  },
+  {
+    "revision": "f29d2b8559699b6beb5b29b25b8bc572",
+    "url": "fonts/montserrat-v12-latin-regular.woff"
+  },
+  {
+    "revision": "cdf5825996af9d722b115f7baf44ac61",
+    "url": "fonts/montserrat-v12-latin-regular.ttf"
+  },
+  {
+    "revision": "cc10ff13acb629e2ef6ad60833448475",
+    "url": "img/montserrat-v12-latin-regular.cc10ff1.svg"
+  },
+  {
+    "revision": "79982cd1f74c6fa7451bf9b37ead09ff",
+    "url": "fonts/montserrat-v12-latin-700.woff2"
+  },
+  {
+    "revision": "957e93fbbe131a59791cd820d98b7109",
+    "url": "fonts/montserrat-v12-latin-700.woff"
+  },
+  {
+    "revision": "410890af1dc7ecd3151b4c88b0a3945f",
+    "url": "fonts/montserrat-v12-latin-700.ttf"
+  },
+  {
+    "revision": "30b7aff0fafcb6cb09ede0ff2c8c8e51",
+    "url": "img/montserrat-v12-latin-700.30b7aff.svg"
+  },
+  {
+    "revision": "53536e374b284393d52a502d1a678042",
+    "url": "index.html"
+  },
+  {
+    "revision": "10e9fd40f97227c4ff4362b5f9a5a2ae",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "287419f7ecaf619dce044c60c9df248e",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "1ff50d3c20f38522e7ae8904ba7db547",
+    "url": "favicon.ico"
+  }
+]);
