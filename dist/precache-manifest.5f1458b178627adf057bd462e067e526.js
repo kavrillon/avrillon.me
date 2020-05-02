2fba0a6eda79ce27190abc8748687a77a563a13f
Kevin Avrillon <kevin@avrillon.me>
2020-05-02 23:04:42 +0200

    chore(): generate files for version 2.10.1

---
+++ dist/precache-manifest.5f1458b178627adf057bd462e067e526.js
@@ -0,0 +1,111 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aec47c8336156a860a3",
+    "url": "css/critical.ed62f9c.css"
+  },
+  {
+    "revision": "d9e01c447167c783d4562738989fe959",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "95bcc17d5781887ae004ffdc6963b832",
+    "url": "fonts/montserrat-v12-latin-200.eot"
+  },
+  {
+    "revision": "52f4c7bb2db43387e6a03d82d2cb9be5",
+    "url": "fonts/montserrat-v12-latin-200.ttf"
+  },
+  {
+    "revision": "edbce16a90aa22c297a0307b85789837",
+    "url": "fonts/montserrat-v12-latin-200.woff"
+  },
+  {
+    "revision": "444ae007121264bc1969d49b4031f9b2",
+    "url": "fonts/montserrat-v12-latin-200.woff2"
+  },
+  {
+    "revision": "040482ce0f8a3aee1f0568d9c50ba55d",
+    "url": "fonts/montserrat-v12-latin-700.eot"
+  },
+  {
+    "revision": "410890af1dc7ecd3151b4c88b0a3945f",
+    "url": "fonts/montserrat-v12-latin-700.ttf"
+  },
+  {
+    "revision": "957e93fbbe131a59791cd820d98b7109",
+    "url": "fonts/montserrat-v12-latin-700.woff"
+  },
+  {
+    "revision": "79982cd1f74c6fa7451bf9b37ead09ff",
+    "url": "fonts/montserrat-v12-latin-700.woff2"
+  },
+  {
+    "revision": "29c1d31f7d9bc4f5c1841eb14fbf5cd7",
+    "url": "fonts/montserrat-v12-latin-regular.eot"
+  },
+  {
+    "revision": "cdf5825996af9d722b115f7baf44ac61",
+    "url": "fonts/montserrat-v12-latin-regular.ttf"
+  },
+  {
+    "revision": "f29d2b8559699b6beb5b29b25b8bc572",
+    "url": "fonts/montserrat-v12-latin-regular.woff"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "fonts/montserrat-v12-latin-regular.woff2"
+  },
+  {
+    "url": "icon_192x192.3ffd4fff76c20e51ac521c5f55686d79.png"
+  },
+  {
+    "url": "icon_512x512.1cb5acb2a61eec573ebbc03ffe81b1f3.png"
+  },
+  {
+    "revision": "d931475b488bcd57a1b9b6f3c7e1ed5d",
+    "url": "img/apple-touch-icon.a0a79bb.png"
+  },
+  {
+    "revision": "fa31f7100e07c75a65de7f1318c9a0eb",
+    "url": "img/favicon-16x16.2892ea0.png"
+  },
+  {
+    "revision": "4cf48158babf24ab728864e490b4f76a",
+    "url": "img/favicon-32x32.cd1e1f8.png"
+  },
+  {
+    "revision": "c4bce4a7535147c26ff98c30cf7f045d",
+    "url": "img/montserrat-v12-latin-200.e683737.svg"
+  },
+  {
+    "revision": "30b7aff0fafcb6cb09ede0ff2c8c8e51",
+    "url": "img/montserrat-v12-latin-700.5cb7271.svg"
+  },
+  {
+    "revision": "cc10ff13acb629e2ef6ad60833448475",
+    "url": "img/montserrat-v12-latin-regular.c0c90d5.svg"
+  },
+  {
+    "revision": "93bfaaef725a90430b66721acf61ee17",
+    "url": "img/og-image.53f30f3.jpg"
+  },
+  {
+    "revision": "2446175b4f9c154c5a1b999e81117786",
+    "url": "img/safari-pinned-tab.340e3d0.svg"
+  },
+  {
+    "revision": "a95dfbaa2a627c9f5b12316d8b3f50a1",
+    "url": "index.html"
+  },
+  {
+    "revision": "b614ceabf6ae05c09ff4",
+    "url": "js/app.ed62f9c.js"
+  },
+  {
+    "revision": "5aec47c8336156a860a3",
+    "url": "js/critical.ed62f9c.js"
+  },
+  {
+    "url": "manifest.89ada0fec72bc482aefdbe0daee01231.json"
+  }
+]);
---
+++ dist/sw.js
@@ -0,0 +1,27 @@
+importScripts("precache-manifest.5f1458b178627adf057bd462e067e526.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false,
+});
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.routing.registerRoute(/\.(?:js|css|html)$/, new workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.googleAnalytics.initialize();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
