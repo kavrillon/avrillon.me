86d69209fe6ef7b3407ddaddabec68128299f9c1
Kevin Avrillon <kevin@avrillon.me>
2022-06-28 18:02:20 +0200

    GitHub Pages Deployment: 2.11.0

---
+++ sw.js
@@ -0,0 +1,25 @@
+importScripts("precache-manifest.75d0ac41854e351bb8e0a044bb675539.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false,
+});
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.routing.registerRoute(/\.(?:js|css|html)$/, new workbox.strategies.StaleWhileRevalidate());
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
